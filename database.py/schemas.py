99ac4c4b630a32bad45c6d8bdf2f50772d72475d
GivaGolf <givanildoti@live.com>
2026-04-01 19:34:40 -0300

    Primeiro commit

---
+++ database.py/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr
+
+class ClienteCreate(BaseModel):
+    nome: str
+    email: EmailStr
+
+class ClienteResponse(BaseModel):
+    id: int
+    nome: str
+    email: EmailStr
+
+    class Config:
+        from_attributes = True
